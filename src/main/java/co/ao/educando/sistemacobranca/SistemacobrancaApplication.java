35f687bd5b525a0d876dfa8dd434140eb0ca5122
Djanilson Lazaro <lazarodjanilson@gmail.com>
2021-04-22 10:12:20 +0100

    Projeto criado

---
+++ src/main/java/co/ao/educando/sistemacobranca/SistemacobrancaApplication.java
@@ -0,0 +1,13 @@
+package co.ao.educando.sistemacobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemacobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemacobrancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ao/educando/sistemacobranca/SistemacobrancaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ao.educando.sistemacobranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemacobrancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
